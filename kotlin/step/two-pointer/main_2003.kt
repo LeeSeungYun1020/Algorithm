713d52374b21c8dc3e751ea1858ad80fd5b4974b
Lee Seung Yun <ileilliat@gmail.com>
2022-04-24 08:47:55 +0900

    Two pointer - 2003

--- kotlin/step/two-pointer/main_2003.kt
+++ kotlin/step/two-pointer/main_2003.kt
@@ -6,12 +6,12 @@ fun main() {
     var sum = 0L
     var count = 0
     while (true) {
-        if (sum >= M) {
+        if (sum >= M) { // 합이 지정값보다 크거나 같으면 앞에 값 빼야 함
             sum -= num[low]
             low++
-        } else if (high == N.toInt()) {
+        } else if (high == N.toInt()) { // high가 범위 벗어난 경우 반복 종료
             break
-        } else {
+        } else { // 합이 지정값보다 작은 경우, 다음 값 선택
             sum += num[high]
             high++
         }
