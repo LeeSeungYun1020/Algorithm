2363cb5736351e150d60969eeb988b0839e7f0b3
Lee Seung Yun <ileilliat@gmail.com>
2022-08-05 21:33:47 +0900

    Kit - 단어 변환

--- kotlin/kit/dfs-bfs/단어 변환.kt
+++ kotlin/kit/dfs-bfs/단어 변환.kt
@@ -16,6 +16,7 @@ class Solution {
         }
         return false
     }
+
     fun solution(begin: String, target: String, words: Array<String>): Int {
         if (target !in words)
             return 0
