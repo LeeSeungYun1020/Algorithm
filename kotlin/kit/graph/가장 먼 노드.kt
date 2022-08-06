b86edb0b804eae78bc01c43d205295c0bf1f937e
Lee Seung Yun <ileilliat@gmail.com>
2022-08-06 13:26:04 +0900

    Kit - 가장 먼 노드

--- kotlin/kit/graph/가장 먼 노드.kt
+++ kotlin/kit/graph/가장 먼 노드.kt
@@ -1,5 +1,6 @@
 class Solution {
     fun solution(n: Int, edge: Array<IntArray>): Int {
+        // 메모리 초과 회피 위해 그래프에 해당 값을 넣고 순회하여 조회 (실행 시간은 증가)
         val graph = List(n) { mutableListOf<Int>() }
         for (e in edge) {
             graph[e[0] - 1].add(e[1] - 1)
