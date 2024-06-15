9b5d34b6b1274ce09054fd42235afc6ff5b5284a
Lee Seung Yun <ileilliat@gmail.com>
2024-06-16 08:33:25 +0900

    round243 - Mixtape Management 수정

--- kotlin/round/243/20913.kt
+++ kotlin/round/243/20913.kt
@@ -1,28 +1,10 @@
 fun main() {
-    val n = readln().toInt()
-    val windows = readln().split(" ").map { NumberFile(it.toInt()) }
-
-    for (target in 1..n) {
-        var isMet = false
-        windows.forEach {
-            when {
-                it.isFixed -> return@forEach
-                it.order == target -> {
-                    isMet = true
-                    it.isFixed = true
-                    it.nameBuilder.append(2)
-                }
-                !isMet -> {
-                    it.nameBuilder.append(1)
-                }
-                isMet -> {
-                    it.nameBuilder.append(2)
-                }
-
-            }
+    readln()
+    println(buildString {
+        readln().split(" ").forEachIndexed { index, intString ->
+            append(index + 100)
+            append("0".repeat(intString.toInt() - 1))
+            append(" ")
         }
-    }
-    println(windows.joinToString(separator = " ") { it.nameBuilder.toString() })
+    })
 }
-
-class NumberFile(val order: Int, var nameBuilder: StringBuilder = StringBuilder(), var isFixed: Boolean = false)
