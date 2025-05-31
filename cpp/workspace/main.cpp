bec3cb733d62062839a4c2398b1fbd7f825d1375
Lee Seung Yun <ileilliat@gmail.com>
2025-05-31 11:44:27 +0900

    main 파일 정리

--- cpp/workspace/main.cpp
+++ cpp/workspace/main.cpp
@@ -1,42 +1,11 @@
-#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
 
-    int* input = new int[n];
-    int max = 0;
-    for (int i = 0; i < n; ++i) {
-        cin >> input[i];
-        if (input[i] > max)
-            max = input[i];
-    }
-
-    auto* divisor = new long long[max + 1];
-    divisor[0] = 0;
-    for (int i = 1; i <= max; ++i) {
-        divisor[i] = 1;
-    }
-
-    for (int i = 2; i <= max; ++i) {
-        for (int j = 1; j <= max; ++j) {
-            long long k = i;
-            if (k * j <= max) {
-                divisor[i * j] += i;
-            } else break;
-        }
-    }
-
-
-    for (int i = 2; i <= max; ++i) {
-        divisor[i] += divisor[i-1];
-    }
-
-    for (int i = 0; i < n; ++i) {
-        cout << divisor[input[i]] << '\n';
-    }
-
-    return 0;
+  return 0;
 }
