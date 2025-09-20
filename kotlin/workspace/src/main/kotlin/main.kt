2017d98ce35019ab3a4ffcab5fc8f44b00ffae5b
Lee Seung Yun <ileilliat@gmail.com>
2025-09-20 10:45:36 +0900

    round303 - CPU 벤치마킹

--- kotlin/workspace/src/main/kotlin/main.kt
+++ kotlin/workspace/src/main/kotlin/main.kt
@@ -1,3 +1,13 @@
-fun main() {
+const val MOD = 1_000_000_007L
 
+fun main() {
+    readln()
+    readln().split(" ")
+        .map { it.toLong() }
+        .fold(0L to 0L) { (acc, sum), input ->
+            val n = ((acc + 1) % MOD * input % MOD) % MOD
+            n to (sum + n) % MOD
+        }
+        .second
+        .run(::println)
 }
